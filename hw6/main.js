27379b77beee922f172da23490e8be791eeaf965
Yevhenii Babenko <yevheniibabenko@gmail.com>
2021-07-11 23:19:53 +0300

    some code for testing

--- hw6/main.js
+++ hw6/main.js
@@ -113,3 +113,126 @@ x.bar()
 new foo()
 
 foo.apply({}) */
+/**
+ * 
+ * @param {string} value
+ */
+/* function getValidator() {
+    return {
+        shouldHaveOnlyLatin: false,
+        shouldHaveDitgits: false,
+        latinOnly() {
+            this.shouldHaveOnlyLatin = true
+        },
+        hasDigits() {
+            this.shouldHaveDitgits = true
+        },
+        validate(value) {
+            let isValid = true
+            if (this.shouldHaveOnlyLatin) {
+                isValid = value.split('').every(
+                    c => {
+                        const lowerChar = c.toLowerCase()
+                        return 'a' <= lowerChar && lowerChar <= 'z'
+                    }
+                )
+            }
+            if (this.shouldHaveDitgits) {}
+            return isValid
+        }
+    }
+}
+
+const validator = getValidator()
+validator.latinOnly()
+validator.hasDigits()
+
+const value = 'foo'
+
+const result = validator.validate(value)
+
+console.log(value, result)
+ */
+
+
+/* const phone = {
+    price: 150,
+    nameOfGood: 'iPhone',
+    foo: {
+        bar: 10,
+    }
+};
+
+const {
+    price: phonePrice,
+    nameOfGood,
+    foo,
+    ...otherProperties
+} = phone */
+
+// const { price, nameOfGood } = phone ;
+/* const { bar } = phone.foo; */
+// console.log(price, nameOfGood, bar)
+/* console.log(otherProperties)
+
+const arr = [1, 2 , 3 ,4, 5];
+
+const [, ,second, ...others] = arr
+console.log(arr, second)
+console.log(others) */
+
+/* let a = 10;
+let b = 20;
+
+[a, b] = [b, a];
+console.log('a', a);
+console.log('b', b)
+
+
+const arr = [1, 2, 3, 4, 5, 6] */
+
+/* const reducedArr = arr.reduce(function (sum, current) {
+    debugger;
+    return sum + current
+}, 0);
+document.writeln(reducedArr); */
+/* 
+const callback = (prev, x, index, aRr) => {
+    console.log(`Prev: ${prev}, item: ${x}, index: ${index}`, aRr);
+    return prev + x;
+}
+
+const sum = arr.reduce(callback, 50);
+
+console.log(sum);
+
+const users = [
+    {name: 'Str', age: 36},
+    {name: 'Arr', age: 46},
+    {name: 'Nmb', age: 56}
+]; */
+/* 
+const users = [
+    {naMe: 'Str', age: 36},
+    {naMe: 'Arr', age: 46},
+    {naMe: 'Nmb', age: 56}
+]; */
+/* for (let user of users) {
+    result[user.name] = user.age;
+} */
+
+/* const callback = ((prev, {naMe, age}) => ({...prev, [naMe]:age}));
+const result = users.reduce(callback, {});
+console.log(result);
+
+const str = '223';
+const arr1 = [...str].map(n => + n);
+console.log(arr1);
+const ourRes = arr1.reduce((a, b) => a + b, 0);
+console.log(ourRes)
+
+function digital_root(n) { 
+    return [...n.toString()].map(s => + s).reduce((a,b) => a+b)
+}
+
+console.log(digital_root(16)) */

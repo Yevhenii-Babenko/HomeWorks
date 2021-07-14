af9235edc1866308646b0c050cc423c228518991
Yevhenii Babenko <yevheniibabenko@gmail.com>
2021-07-14 17:25:48 +0300

    added some code from tutorial

--- hw6/main.js
+++ hw6/main.js
@@ -1,14 +1,173 @@
+const students = [{
+    name: "Tanya",
+    course: 3,
+    subjects: {
+      math: [4, 4, 3, 4],
+      algorithms: [3, 3, 3, 4, 4, 4],
+      data_science: [5, 5, 3, 4]
+    }
+  }, {
+    name: "Victor",
+    course: 4,
+    subjects: {
+      physics: [5, 5, 5, 3],
+      economics: [2, 3, 3, 3, 3, 5],
+      geometry: [5, 5, 2, 3, 5]
+    }
+  }, {
+    name: "Anton",
+    course: 2,
+    subjects: {
+      statistics: [4, 5, 5, 5, 5, 3, 4, 3, 4, 5],
+      english: [5, 3],
+      cosmology: [5, 5, 5, 5]
+    }
+  }];
+
+/* const userName = new Object()
+userName.name = 'Jogh';
+userName.age = 35;
+userName.isAdmin = true;
+console.log(userName);
+delete userName.isAdmin
+userName.name = 'Rico'
+console.log(userName); */
+
+/* console.log(userName); */
+/* 
+let someStr = {};
+someStr['cool man'] = true; */
+
+// alert(someStr['cool man']);
+
+/* const userSurname = {};
+console.log(userSurname)
+
+let key = 'like birds';
+someStr[key] = prompt('question?');
+console.log(someStr)
+
+let fruit = prompt('Fruit to buy?', 'orange');
+let bag = {
+    [fruit]: 5,
+}
+
+alert(bag.orange); */
+/* "use strict"; */
+
+/* function mUs(name, age) {
+    return {
+        name, 
+        age
+    }
+}
+
+let ourUser = mUs('Rick', 30)
+alert(ourUser.name);
+alert(ourUser.age);
+alert('age' in ourUser); */
+
+/* let codes = {
+    '49': 'Germ',
+    '41': 'Swith',
+    '44': 'Great',
+    '1': 'USA'
+}
+
+for (let code in codes) {
+    console.log(code);
+}
+ */
+
+//Object.assign
+
+/* let user = {
+    name: 'Ivan',
+    age: 30
+}
+
+let clone = new Array();
+
+for(let key in user){
+    clone[key] = user[key];
+}
+
+
+console.log(clone.name, clone.age) */
+
+/* let user = {
+    name: 'Ivan',
+    sizes: {
+        height: 182,
+        width: 50
+    }
+};
+let perm1 = {canView: true};
+let perm2 = {canEdit: true};
+
+console.log(Object.assign(user, perm1, perm2))
+
+user.sizes.width++
+console.log(Object.assign({}, user)) */
+/* 
+const user = {
+    name: 'Bogdan',
+    status: 'Lector',
+    createdBy: 'Ivan',
+    someInfo: {
+        age: 30,
+        lenght: 170
+    }
+}
+
+const {name, status,...other} = user;
+console.log(`${name} and ${status}`);
+console.log(`other = ${JSON.stringify(other)}`)
+
+
+const newUser = {name};
+console.log(`newUser = ${JSON.stringify(newUser)}`) */
+
+/* const sub = {
+    m: true,
+    e: false,
+    c: false,
+}
+
+const obj = {
+    n: 'Gans',
+    adr: 'New Line str'
+}
+
+const gena = {
+    ...sub,
+    ...obj,
+}
+console.log(`All info of: ${JSON.stringify(gena)}`);
+console.log(Object.keys(gena));
+console.log(Object.values(gena));
+console.log(Object.entries(gena));
+
+const student = {
+    marks: [5, 4, 3],
+    addNewMark(mark) {
+        return this.marks.push(mark)
+    }
+}
+student.addNewMark(5)
+console.log(student.marks) */
+
 /* function doStuff() {
     const obj1 = {
         foo: 10
-    } 
-    
+    }
+
     const obj2 = {
         foo: 20
     }
-    
+
     obj1.foo = 30;
-    
+
     console.log(obj1, obj2);
 }
 
@@ -102,7 +261,7 @@ function hush() {
 
 foo()
 
-var x = { 
+var x = {
     bar: foo,
     red : hush
  }
@@ -112,11 +271,7 @@ x.bar()
 
 new foo()
 
-foo.apply({}) */
-/**
- * 
- * @param {string} value
- */
+
 /* function getValidator() {
     return {
         shouldHaveOnlyLatin: false,
@@ -196,7 +351,7 @@ const arr = [1, 2, 3, 4, 5, 6] */
     return sum + current
 }, 0);
 document.writeln(reducedArr); */
-/* 
+/*
 const callback = (prev, x, index, aRr) => {
     console.log(`Prev: ${prev}, item: ${x}, index: ${index}`, aRr);
     return prev + x;
@@ -211,7 +366,7 @@ const users = [
     {name: 'Arr', age: 46},
     {name: 'Nmb', age: 56}
 ]; */
-/* 
+/*
 const users = [
     {naMe: 'Str', age: 36},
     {naMe: 'Arr', age: 46},
@@ -231,8 +386,55 @@ console.log(arr1);
 const ourRes = arr1.reduce((a, b) => a + b, 0);
 console.log(ourRes)
 
-function digital_root(n) { 
+function digital_root(n) {
     return [...n.toString()].map(s => + s).reduce((a,b) => a+b)
 }
 
 console.log(digital_root(16)) */
+
+// Global Scope
+
+/* console.log(this); */
+// Browser this -> Window
+// NodeJs -> Global
+
+// Function Scope
+
+/* function func(){ */
+    // this -> Window || Global for NodeJS;
+/* } */
+
+// Object
+
+//Object.method() // {
+    // this -> Object;
+//}
+
+/* const user = {
+    name: 'Name',
+    surName: 'SurName',
+    getFullName() {
+        return this.name + ' ' + this.surName
+    }
+}
+
+console.log(user.getFullName()) */
+
+
+/*
+// Constructor
+function Constructor () {
+    return this -> new Entity
+} */
+
+/* event Listener
+html.onclick html.onChange html.addEventListener
+function() {
+    this --> htmlElement
+}
+*/
+
+// Arrow function
+/*
+    () => this --> declaring context
+*/

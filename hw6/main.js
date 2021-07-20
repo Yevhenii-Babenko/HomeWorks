28413198675f27dae5054529e3df66541f1ef9bf
Yevhenii Babenko <yevheniibabenko@gmail.com>
2021-07-20 13:22:03 +0300

    refactored hw6

--- hw6/main.js
+++ hw6/main.js
@@ -28,69 +28,76 @@ const students = [{
 1. Створіть функцію getSubjects(students[0] --> ["Math", "Algorithms", "Data science"] - яка повертає список предметів для конкретного студента.
 Зверніть увагу – назву предмету необхідно повертати з великої літери, а _ – замінити на пробіл
 */
-const { subjects } = students[0];
+// const { subjects } = students[0];
 
-function getSubjects(arr) {
-    const keys = Object.keys(arr).map(a => a.slice(0, 1).toUpperCase() + a.slice(1));
-    return keys
+function getSubjects(studentsSubjects) {
+    const { subjects } = studentsSubjects;
+    const keys = Object.keys(subjects).map(a => a.slice(0, 1).toUpperCase() + a.slice(1));
+    return keys.map((item) => item.replace('_',' '))
 }
-const result = getSubjects(subjects);
 document.writeln(`<p style="margin:0; padding:0">1. Створіть функцію getSubjects(students[0] --> ["Math", "Algorithms", "Data science"] - яка повертає список предметів для конкретного студента.
-Зверніть увагу – назву предмету необхідно повертати з великої літери, а _ – замінити на пробіл</p> <br> 
-<p style="margin:0; padding:0; color:green">Результат виклику функції getSubjects() : ${result}</p><br>`)
+Зверніть увагу – назву предмету необхідно повертати з великої літери, а _ – замінити на пробіл</p>
+<p style="margin:10px 0 0; padding:0; color:#097961">Результат виклику функції getSubjects(students[0]) : ${JSON.stringify(getSubjects(students[0]))} </p><br>`)
+
 /* 
 2. Створіть функцію getAverageMark(students[0]) --> 3.79 – яка поверне середню оцінку по усім предметам для переданого студента НЕ МАСИВА СТУДЕНТІВ. Оцінку округліть до 2ого знаку. Можна використовувати функції, написані у попередніх домашніх завданнях :) 
 */
-const marks = Object.values(subjects)
-function getAverageMark(arr) {
-    return (arr.flatMap(x => x).reduce((a, b) => a + b, 0) / arr.flatMap(x => x).length).toFixed(2);
+
+function getAverageMark(studentMarks) {
+    const { subjects } = studentMarks;
+    return (Object.values(subjects).flatMap(x => x).reduce((a, b) => a + b, 0) /  Object.values(subjects).flatMap(x => x).length).toFixed(2)
 }
+
 document.writeln(`<p style="margin:0; padding:0">2. Створіть функцію getAverageMark(students[0]) --> 3.79 – 
 яка поверне середню оцінку по усім предметам для переданого студента НЕ МАСИВА СТУДЕНТІВ. Оцінку округліть до 2ого знаку.
-Можна використовувати функції, написані у попередніх домашніх завданнях :) </p> <br> 
-<p style="margin:0; padding:0; color: #ED4C5C">Результат виклику функції getAverageMark() : ${getAverageMark(marks)}</p><br>`);
+Можна використовувати функції, написані у попередніх домашніх завданнях :)</p>
+<p style="margin:10px 0 0; padding:0; color:#097961">Результат виклику функції getAverageMark(students[0]) : ${getAverageMark(students[0])}</p><br>`);
 
 /*
 3. Створіть функцію getStudentInfo(students[0]) --> { "course": 3, "name": "Tanya", "averageMark": 3.79} – яка повертає інформацію загального виду по переданому студенту (вам знадобиться функція з попереднього завдання). Повинна бути виведена інформація: курс, ім'я, середня оцінка. 
 */
 
-let selectedStudFromArr = students[2];
-function getStudentInfo(selectedStud) {
-    let { course, name } = selectedStud;
-    let key = Object.values(selectedStud.subjects);
-   
+function getStudentInfo(selectedStudent) {
+    let { course, name } = selectedStudent;
     return JSON.stringify({
         course,
         name,
-        averageMark: getAverageMark(key)
+        averageMark: getAverageMark(selectedStudent)
     });
 }
 document.writeln(`<p style="margin:0; padding:0">3. 
 Створіть функцію getStudentInfo(students[0]) --> { "course": 3, "name": "Tanya", "averageMark": 3.79} – яка повертає інформацію загального виду по переданому
-студенту (вам знадобиться функція з попереднього завдання). Повинна бути виведена інформація: курс, ім'я, середня оцінка. Результат виклику функції getStudentInfo() : ${getStudentInfo(selectedStudFromArr)}</p><br>`);
+студенту (вам знадобиться функція з попереднього завдання). Повинна бути виведена інформація: курс, ім'я, середня оцінка.</p> 
+<p style="margin:10px 0 0; padding:0; color:#097961">Результат виклику функції getStudentInfo() : ${getStudentInfo(students[0])}</p><br>`);
+
 /* 
 4. Створіть функцію getStudentsNames(students) --> ["Anton", "Tanya, "Victor"] – яка повертає імена студентів у алфавітному порядку. 
 */
 
-function getStudentsNames(arr) {
-    return arr.map(x => x.name).sort((a, b) => a.localeCompare(b));
+function getStudentsNames(studentlist) {
+    return studentlist.map(studentFromList => studentFromList.name).sort();
+    // .sort((a, b) => a.localeCompare(b));
 }
-document.writeln(`<p style="margin:0; padding:0">4. Ствроіть функцію getStudentsNames(students) --> ["Anton", "Tanya, "Victor"] – яка повертає імена студентів у алфавітному порядку</p> <br> 
-<p style="margin:0; padding:0; color: #097961">Результат виклику функції getStudentsNames() : ${getStudentsNames(students)}</p><br>`);
+document.writeln(`<p style="margin:0; padding:0">4. Ствроіть функцію getStudentsNames(students) --> ["Anton", "Tanya, "Victor"] – яка повертає імена студентів у алфавітному порядку</p>
+<p style="margin:10px 0 0; padding:0; color: #097961">Результат виклику функції getStudentsNames(): ${JSON.stringify(getStudentsNames(students))} - відсортований масив імен студентів у алфавітному порядку</p><br>`);
+
 /*
 5. Створіть функцію getBestStudent(students) --> "Anton" – яка повертає кращого студента зі списку по показнику середньої оцінки.
 */
-function getBestStudent(arr) {
-    const studObj = arr.map(student => ({
+function getBestStudent(studentsList) {
+    const studlistWithObj = studentsList.map((student) => ({
         name: student.name,
-        marks: getAverageMark(Object.values(student.subjects)),
-    }))
-    return studObj;
+        marks: getAverageMark(student),
+    }));
+    const bestStudentMark = studlistWithObj.reduce((acc, item) => ({
+        name: item.name,
+        maxMark: Math.max(...studlistWithObj.map(studMark => studMark.marks))
+    }));
+    return bestStudentMark.name;
 }
-
-console.log(getBestStudent(students))
-getBestStudent(students)
-
+// console.log(getBestStudent(students))
+document.writeln(`<p style="margin:0; padding:0">5. Створіть функцію getBestStudent(students) --> "Anton" – яка повертає кращого студента зі списку по показнику середньої оцінки.</p>
+<p style="margin:10px 0 0; padding:0; color: #097961">Результат виклику функції getBestStudent(students): ${JSON.stringify(getBestStudent(students))}</p><br>`);
 /*
 6. Створіть функцію calculateWordLetters("тест") --> { "т": 2, "е": 1, "с": 1 } – яка повертає обє'кт, в якому ключі це букви у слові, а значення – кількість їх повторень.
 */
@@ -105,8 +112,8 @@ function calculateWordLetters(word) {
         return acc
     }, {}))
 }
-document.writeln(`<p style="margin:0; padding:0">6.  Створіть функцію calculateWordLetters("тест") --> { "т": 2, "е": 1, "с": 1 } – яка повертає обє'кт, в якому ключі це букви у слові, а значення – кількість їх повторень.</p><br>
- <p style="margin:0; padding:0; color: #097961">Результат виклику функції calculateWordLetters("тест") : ${calculateWordLetters(str)}</p>`);
+document.writeln(`<p style="margin:0; padding:0">6. Створіть функцію calculateWordLetters("тест") --> { "т": 2, "е": 1, "с": 1 } – яка повертає обє'кт, в якому ключі це букви у слові, а значення – кількість їх повторень.</p>
+ <p style="margin:10px 0 0; padding:0; color: #097961">Результат виклику функції calculateWordLetters("тест") : ${calculateWordLetters(str)}</p>`);
 
 
 

955088ef3e52d48d0f741f71d13e6279a92d60cc
Yevhenii Babenko <yevheniibabenko@gmail.com>
2021-07-26 23:55:27 +0300

    added hw12 in status ready

--- hw17/src/App.js
+++ hw17/src/App.js
@@ -12,23 +12,56 @@ const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e6
         }
 } */
 
-function App(props) {
+const postOwner = {
+  name: "Anakin skywalker",
+  photo: ANAKIN_IMAGE,
+  nickname: "@dart_vader",
+  date: "26 лют.",
+
+};
+const contText = {
+  content: "WTF? Who is Ray? Why she is Skywalker? Luke...?",
+  photo: RAY_IMAGE,
+};
+
+function App() {
   return (
     <div className="post" style={{backgroundColor: "#15202B"}}>
-      <img 
-        className="author-img"
-        src={ANAKIN_IMAGE}
-        alt="Anakin skywalker" />
-        <div className="post-content">
-          <div className="post-text"></div>
-              <img 
-                className="content-img" 
-                  src={RAY_IMAGE}
-                  alt="Ray"
-                />
-          </div>
+      <Author {...postOwner} />
+      <PostContent {...contText} />
       </div>
   );
+}
+
+const Author = ({name, photo, nickname, date}) => {
+  return(
+    <div className="post-author">
+        <img 
+        className="author-img"
+        src={ photo }
+        alt={ name } />
+        <div className="post-container">
+          <h4 className="post-container__header">{ name }</h4>
+          <p className="post-container__p">{ nickname }</p>
+          <p className="post-container__p">{ date }</p>
+        </div>
+      </div>
+  )
+}
+
+const PostContent = ({ content, photo }) => {
+  return(
+    <div className="post-content">
+      <div className="post-text">
+        <p className="post-text_p">{ content }</p>
+      </div>
+        <img 
+          className="content-img" 
+          src={ photo }
+          alt="Ray"
+          />
+      </div>
+  )
 }
 
 export default App;

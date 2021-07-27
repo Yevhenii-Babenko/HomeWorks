25543ab1902ce16b3958497c602e61dbc5b2ac85
Yevhenii Babenko <yevheniibabenko@gmail.com>
2021-07-27 10:57:31 +0300

    delete extra code in hw12

--- hw17/src/App.js
+++ hw17/src/App.js
@@ -3,15 +3,6 @@ import './App.css';
 const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg";
 const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
 
-/* const postContent = {
-        name: "Anakin skywalker",
-        nickname: "@dart_vader",
-        textContent: {
-            content: "WTF? Who is Ray? Why she is Skywalker? Luke...?",
-            date: "26 лют."
-        }
-} */
-
 const postOwner = {
   name: "Anakin skywalker",
   photo: ANAKIN_IMAGE,
